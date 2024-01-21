58108e047c65d8987401ae4eae71b8a1b83ab1fa
João Pedro Martins Forte <jpforte2004@gmail.com>
2024-01-21 16:41:43 -0300

    findAll implementation in DepartmentDaoJDBC class

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -12,5 +14,12 @@ public class Program2 {
         System.out.println("=== TEST 1: findById ====");
         Department dep = departmentDao.findById(1);
         System.out.println(dep);
+
+        System.out.println("\n=== TEST 2: findAll ====");
+        List<Department> list = departmentDao.findAll();
+        
+        for(Department department : list){
+            System.out.println(department);
+        }
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -70,8 +71,28 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
     @Override
     public List<Department> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement("SELECT * FROM department ORDER BY Name");
+            rs = st.executeQuery();
+
+            List<Department> list = new ArrayList<>();
+
+            while(rs.next()){
+                Department obj = instantiateDepartment(rs);
+                list.add(obj);
+            }
+
+            return list;
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
 }
